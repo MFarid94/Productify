b96646ffee11bb0377fe46467d03cf500e51847f
MFarid94 <MFHosni94@gmail.com>
2026-05-14 11:35:46 +0300

    Set up Drizzle ORM with PostgreSQL configuration and initial schema

---
+++ Backend/src/db/index.ts
@@ -0,0 +1,25 @@
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
+import { ENV } from "../config/env";
+import * as schema from "./schema";
+
+if(!ENV.DATABASE_URL) {
+    throw new Error("DATABASE_URL is not defined in environment variables");
+}
+
+// initialize PostgreSQL connection pool
+const pool = new Pool({
+    connectionString: ENV.DATABASE_URL,
+});
+
+// log when first connection is established
+pool.on("connect", () => {
+    console.log("Database connection established");
+})
+
+// log when an error occurs on the pool
+pool.on("error", (err) => {
+    console.error("Database connection error", err);
+});
+
+export const db = drizzle({client: pool, schema});
