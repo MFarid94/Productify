c6577aa6d08a6468d5dea2e5b0e177fcf63ae9a1
MFarid94 <MFHosni94@gmail.com>
2026-06-14 22:53:20 +0300

    Fix interceptor registration logic in useAuthReq to prevent multiple registrations

--- Frontend/src/hooks/useAuthReq.js
+++ Frontend/src/hooks/useAuthReq.js
@@ -2,10 +2,15 @@ import { useAuth } from "@clerk/react"
 import { useEffect } from "react"
 import api from "../lib/axios";
 
+let isInterceptorRegistered = false;
+
 function useAuthReq() {
     const { isSignedIn, getToken, isLoaded } = useAuth();
 
     useEffect(() => {
+        if (isInterceptorRegistered) return;
+        isInterceptorRegistered = true;
+
         const interceptor = api.interceptors.request.use(async (config) => {
             if (isSignedIn) {
             try {
@@ -23,6 +28,7 @@ function useAuthReq() {
 
         return () => {
             api.interceptors.request.eject(interceptor);
+            isInterceptorRegistered = false;
         };
     }, [isSignedIn, getToken]);
 
