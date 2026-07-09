fcdbcc9e5e179d6f74ac569fcec2c5d5f12b4c48
MFarid94 <MFHosni94@gmail.com>
2026-07-09 11:54:15 +0300

    fix: update API URL handling and routing for production environment

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,8 +5,4 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  define: {
-    // This physically overwrites the variable with your exact backend string during the build process
-    'import.meta.env.VITE_API_URL': JSON.stringify('hhttps://productify-zos97.sevalla.app')
-  }
 })
