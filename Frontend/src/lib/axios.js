2c45352d686a82a6d7066184d6886159dd3a5706
MFarid94 <MFHosni94@gmail.com>
2026-07-09 11:07:58 +0300

    fix: update axios baseURL to use a hardcoded value

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: "https://productify-zos97.sevalla.app",
     withCredentials: true,
 })
 
